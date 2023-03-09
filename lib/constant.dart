fae77dc214d16757905499bd273518b4f2d28753
Eduardo Silva Borges <edusborges99@hotmail.com>
2023-03-09 10:36:48 -0300

    feature/endpoint-crud-client

---
+++ lib/constant.dart
@@ -0,0 +1,17 @@
+class Constant {
+  // ASSETS
+  // static const String assetLogoApp = 'assets/x_capital_test_mobile.png';
+  // static const String assetSplash = 'assets/splash.png';
+
+  // BASE PROTOCOL
+  static const String baseProtocol = 'http://';
+  static const String baseProtocolS = 'https://';
+
+  // BASE DOMAIN
+  static const String baseDomain = 'provisorio.sousmol.com.br';
+
+  // ENDPOINTS
+  // https://apitestexcapital.herokuapp.com/api/v1/balance
+  static const String getBalance =
+      "${baseProtocolS + baseDomain}/vagaflutter/crud-cliente/";
+}
